baaf06cda498acdba57c6571806cacbcf3985043
Sky020 <2397860h@student.gla.ac.uk>
2020-11-21 16:18:17 +0000

    remove old files

--- src/Components/Challenge.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Challenge = () => {
-  return <div id="challenge">This is some challenge content...</div>;
-};
-
-export default Challenge;
--- src/Components/Lesson.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Lesson = () => {
-  return <div id="lesson">This is some lesson content...</div>;
-};
-
-export default Lesson;
