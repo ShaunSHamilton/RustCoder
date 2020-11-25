e98bdd2b018b17788f61f8c26f16d318af98069c
Sky020 <2397860h@student.gla.ac.uk>
2020-11-25 20:53:27 +0000

    ➕add: init with events to listen on app

--- src-tauri/src/cmd.rs
+++ src-tauri/src/cmd.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -7,4 +10,18 @@ pub enum Cmd {
   // multiple arguments are allowed
   // note that rename_all = "camelCase": you need to use "myCustomCommand" on JS
   MyCustomCommand { argument: String },
+}
+
+pub fn read_file(&file_path: String) -> std::io::Result<()> {
+    let file = File::open(file_path)?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+    Ok(())
+}
+
+pub fn write_file(&file_name: String, &contents: String) -> std::io::Result<()> {
+    let mut file = File::create(file_name)?;
+    file.write_all(contents)?;
+    Ok(())
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -15,18 +15,48 @@ struct Reply {
 fn main() {
   tauri::AppBuilder::new()
     .setup(|_webviews, _source| {
-    // use cmd::Cmd::*;
-      // const webviews = webview.copy();
-      // let mut webview = webviews.as_mut();
-      // tauri::event::listen(String::from("codeSubmit"), move |msg| {
-      //   println!("got js-event with message '{:?}'", msg);
-      //   let reply = Reply {
-      //     data: "// Running tests...\n// Test results".to_string(),
-      //   };
+      use tauri::event::{listen, emit};
+      use tauri_api::dialog::{pick_folder, document_dir}
+      use cmd::Cmd::*;
+      const webviews = webview.copy();
+      let mut webview = webviews.as_mut();
+      let mut file_name;
 
-      //   tauri::event::emit(&mut webview, String::from("rust-event"), Some(reply))
-      //     .expect("failed to emit");
-      // });
+      let mut lesson_folder_path;
+
+      // When user initialises the app, a dialog opens - asking where the user has
+      // stored the 'lesson_files'
+      listen(String::from("initialiseEnv"), move |msg| {
+        println!("Initialising Environment...");
+        let default_path = document_dir();
+        lesson_folder_path: Result<Response> = pick_folder(default_path);
+      })
+
+      listen(String::from("setFile"), move |msg| {
+        println!("Setting file...");
+        file_name = msg.copy();
+      })
+
+      listen(String::from("codeSubmit"), move |msg| {
+        println!("JS-event with message '{:?}'", msg);
+        let reply = Reply {
+          data: "Saved code to file...".to_string(),
+        };
+
+        write_file(file_name, &msg)
+
+        emit(&mut webview, String::from("rust-success"), Some(reply))
+          .expect("failed to emit");
+      });
+      listen(String::from("codeRead"), move |msg| {
+        println!("got js-event with message '{:?}'", msg);
+        let reply = Reply {
+          data: "// Running tests...\n// Test results".to_string(),
+        };
+
+        emit(&mut webview, String::from("rust-event"), Some(reply))
+          .expect("failed to emit");
+      });
     })
     .build()
     .run();
